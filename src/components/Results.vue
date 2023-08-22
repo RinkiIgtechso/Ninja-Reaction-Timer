6c02601b2f09210a7dd3b5e17c011eb417a1a0c8
RinkiIgtechso <kumaririnki@igtechso.com>
2023-08-22 11:14:17 +0530

    Ninja reaction timer

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction Time: {{ score }} ms</p>
+    <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 350){
+            this.rank = 'Ninja fingers'
+        }else if(this.score < 500){
+            this.rank = 'Rapid reflexes'
+        }else{
+            this.rank = "snail pace..."
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 2.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
